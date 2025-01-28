e3e88c5326197287e402e99615c5627cce21cddb
Pedrao01 <149744853+Pedrao01@users.noreply.github.com>
2025-01-28 11:06:18 -0300

    implementação de funções

--- gerar_arquivo.py
+++ gerar_arquivo.py
@@ -12,3 +12,18 @@ def manipular_arquivo(nome_arquivo, informacoes_aluno):
                 arquivo.write(c)
                 arquivo.write(';')
             arquivo.write('\n')
+
+
+def devedores(nome_arquivo, informacoes_aluno, posicao):
+    if posicao == 1:
+        with open(nome_arquivo, 'w+') as arquivo:
+            for c in informacoes_aluno:
+                arquivo.write(c)
+                arquivo.write(';')
+            arquivo.write('\n')
+    else:
+        with open(nome_arquivo, 'a+') as arquivo:
+            for c in informacoes_aluno:
+                arquivo.write(c)
+                arquivo.write(';')
+            arquivo.write('\n')
--- pagamento_pendente.py
+++ pagamento_pendente.py
@@ -1,16 +1,20 @@
 from datetime import datetime, timedelta
+from gerar_arquivo import devedores
 
 
 def tratar(file_name):
     with open(file_name, 'r') as file:
-        for line in file.readlines():
+        cont = 0
+        for num_linha, line in enumerate(file.readlines(), start=1):
             print(line)
             lv = line.split(';')
             lv.pop()
-            pendentes(lv)
+            cont += 1
+            print(cont)
+            pendentes(lv, cont)
 
 
-def pendentes(lista):
+def pendentes(lista, numero_linhas):
     date_start = lista[1].split('.')
     date_start.reverse()
 
@@ -27,8 +31,11 @@ def pendentes(lista):
     print(type(diferenca))
     if diferenca == timedelta(days=31):
         print('dia do pagamento')
+
     if diferenca > timedelta(days=31):
         print('atrasou pagamento')
+        print(numero_linhas)
+        devedores('alunos_com_pagamento_atrasado', lista, numero_linhas)
     print(diferenca)
 
 
